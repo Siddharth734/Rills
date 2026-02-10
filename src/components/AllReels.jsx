8bb59d498fe43a5d498af6b33d5c4e9abaade319
Siddharth734 <siddharthbaghel777@gmail.com>
2026-02-10 18:40:32 +0530

    - Implemented Instagram's like animation manually via useRef - Made index based object for likedReels

--- src/components/AllReels.jsx
+++ src/components/AllReels.jsx
@@ -1,16 +1,37 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import reelsData from "../data/reelsdata";
 
 export default function AllReels() {
-    const [isLiked, setIsLiked] = useState(false);
+    const [likedReels, setLikedReels] = useState({});
+    const heartRefs = useRef({}); 
+
+    function HeartMotion(reelId){
+        const heartElement = heartRefs.current[reelId];
+        if(!heartElement) return;
+        setLikedReels(prev => ({
+            ...prev,
+            [reelId]: !prev[reelId]
+        }));
+        heartElement.style.opacity = 1;
+        heartElement.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)';
+        setTimeout(() => {
+            heartElement.style.transform = 'translate(-50%,-300%)';
+        },600);
+        setTimeout(() => {
+            heartElement.style.opacity = 0;
+        },800);
+        setTimeout(() => {
+            heartElement.style.transform = 'translate(-50%,-50%) scale(0) rotate(60deg)';
+        },1000);
+    }
     
     return (
         <div className="all-reels">
             {
                 reelsData.map(reel => 
-                <div className="reel" key={reel.id} onDoubleClick={() => {setIsLiked(!isLiked)}}>
-                        {/* <img className="main-img" src={reel.mainImg} alt="" /> */}
-                        <video autoPlay loop muted playsInline src={reel.video}></video>
+                <div className="reel" key={reel.id} onDoubleClick={() => {HeartMotion(reel.id)}}>
+                        <img className="main-img" src={reel.mainImg} alt="" />
+                        {/* <video autoPlay loop muted playsInline src={reel.video}></video> */}
                         <div className="bottom">
                             <div className="user">
                                 <img className="user-img" src={reel.user.userImg} alt="" />
@@ -22,8 +43,8 @@ export default function AllReels() {
                         <div className="right">
                             <div className="like">
                                 <h4 className="like-icon">
-                                    <i className={isLiked?"ri-heart-3-fill liked":"ri-poker-hearts-line"}
-                                    onClick={() => {setIsLiked(!isLiked)}}></i>
+                                    <i className={likedReels[reel.id]?"ri-heart-3-fill liked":"ri-poker-hearts-line"}
+                                    onClick={() => {HeartMotion(reel.id)}}></i>
                                 </h4>
                                 <h6>{reel.stats.likes}</h6>
                             </div>
@@ -44,6 +65,10 @@ export default function AllReels() {
                                     <i className="ri-more-2-line"></i>
                                 </h4>
                             </div>
+                        </div>
+                        //stores each heart ele with its respective reel.id
+                        <div className="heart"> 
+                            <i className="ri-heart-3-fill" ref={el => heartRefs.current[reel.id] = el}></i>
                         </div>
                     </div>
                     )
